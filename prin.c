87534543bb2ec2d8d335b84358e8107b21a6e444
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-01-31 16:53:55 +0530

    prin.c

---
+++ prin.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a=5,b=50,c=4,d=50,e,f;
+printf("the two num\t%d\t%d\n",a,b);
+printf("another two num\t%d\t%d\n",c,d);
+e=a-c;
+f=b-d;
+printf("\t%d\t%d",e,f);
+return 0;
+}
