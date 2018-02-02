80afd72cbe6013ec50316b657a07ac379966ffd7
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-02-02 15:59:48 +0530

    arr5.c

---
+++ arr5.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+int main()
+{
+	int i,b[60],s;
+	scanf("%d",&n);
+	for(i=0;i<s;i++)
+	{
+		scanf("%d",&b[i]);
+	}
+	for(i=0;i<s;i++)
+	{
+		printf("%d %d \n",b[i],i+1);
+	}
+	return 0;
+}
