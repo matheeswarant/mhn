2f51c0a706cef2e76b21b3a4bac0939956d0e625
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-02-02 16:46:50 +0530

    mer.c

---
+++ mer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int d[50]={1,4,3,2},n,i;
+for(i=0;i<=n;i++)
+{
+for(j=i+1;j<=n;j++
+{
+if(d[i]>d[j])
+{
+t=d[i];
+d[i]=d[j];
+d[j]=t;
+}
+}
+printf("the sor num is",d[i]\7);
+return 0;
+}
